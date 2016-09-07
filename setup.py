400f54960e5c92308c972faff199c848543ebad0
Krzysztof Babendych <krzysztof@babendych.pl>
2016-09-07 23:12:06 +0200

    Add setup.py to allow pip install and repository cleanup

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+from __future__ import unicode_literals, absolute_import
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='zipstream',
+    version='0.0.1',
+
+    description='Creating zip files on the fly',
+    long_description=long_description,
+
+    url='https://github.com/kbbdy/zipstream',
+
+    author='Krzysztof Babendych',
+    #author_email='not_sure',
+
+    license='BSD',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='zip streaming',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['examples', 'tests']),
+)
+
+
